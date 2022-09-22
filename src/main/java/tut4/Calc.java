40a9d62bfbdc84b619cababb47e5ad5dc4bfb884
zlm <1599546654@qq.com>
2022-09-22 21:04:30 +0800

    251tut4 commit

---
+++ src/main/java/tut4/Calc.java
@@ -0,0 +1,10 @@
+package tut4;
+
+public class Calc {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int subtract(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,15 @@
+package tut4;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestCalc extends TestCase{
+    @Test
+    void testFact(){
+        Assertions.assertEquals(2,Calc.add(1,1));
+
+    }
+}
